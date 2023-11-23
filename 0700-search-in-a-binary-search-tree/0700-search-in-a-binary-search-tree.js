abc616453c88ac7ccd07952a39c51508148d7e6e
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-11-23 20:09:49 +0900

    Time: 66 ms (72.20%), Space: 49 MB (71.60%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+  let currentRoot = root;
+  let result = null;
+  
+  while (true) {
+    if (currentRoot.val == val) {
+      result = currentRoot;
+      
+      break;
+    }
+    
+    if (currentRoot.rigth === null && currentRoot.left === null) {
+      break;
+    }
+    
+    if (currentRoot.right && (val > currentRoot.val)) {
+      currentRoot = currentRoot.right;
+      
+      continue;
+    }
+    
+    if (currentRoot.left && (val < currentRoot.val)) {
+      currentRoot = currentRoot.left;
+      
+      continue;
+    }
+    
+    break;
+  }
+  
+  return result;
+};
