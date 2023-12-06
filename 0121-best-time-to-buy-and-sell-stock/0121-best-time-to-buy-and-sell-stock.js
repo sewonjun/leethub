b4cf49e9f8a2bbfc968fa295870e903ccf740b71
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-12-06 21:05:51 +0900

    Time: 1109 ms (5.05%), Space: 56.6 MB (5.01%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -6,35 +6,32 @@ var maxProfit = function(prices) {
   let curMin = prices[0];
   let benefits = [Math.max(...prices.slice(1)) - curMin];
   let target = 1;
+  
   for (let i = 1; i < prices.length - 1; i++) {
     if (prices[i] > curMin) {
-      console.log("pirce가 curMin보다 큼")
       continue;
     } else {
       curMin = prices[i];
     }
     
     const curMax = Math.max(...prices.slice(i + 1));
+    
     if (curMax < curMin){
-      console.log("min보다 max가 작음");
       continue;
     }
     
     benefits.push(curMax - curMin);
-    console.log(benefits);
     
     let nextMin = null;
     for (let j = i + 1; j < prices.length - 1; j++) {
       if (prices[j] < curMin) {
         target = j
-        console.log(i);
         nextMin = prices[j];
         break;
       }
     }
     
     if (nextMin === null) {
-      console.log("더이상 curMin보다 작은수가 없음");
       break;
     }
   }
