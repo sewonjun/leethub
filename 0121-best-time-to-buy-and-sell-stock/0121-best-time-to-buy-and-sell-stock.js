6ab88974aaaedb186bd7ca68dda41e68a4a0d972
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-12-07 16:41:56 +0900

    Time: 63 ms (90.66%), Space: 51.5 MB (70.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,39 +2,18 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-  let curMin = prices[0];
-  let benefits = [Math.max(...prices.slice(1)) - curMin];
-  let target = 1;
+const maxProfit = function(prices) {
+  let buyIdx = 0;
+  let sellIdx = 1;
+  let benefit = 0;
   
-  for (let i = 1; i < prices.length - 1; i++) {
-    if (prices[i] > curMin) {
-      continue;
-    } else {
-      curMin = prices[i];
-    }
+  while (sellIdx < prices.length) {
+    if (prices[buyIdx] > prices[sellIdx]) buyIdx = sellIdx;
     
-    const curMax = Math.max(...prices.slice(i + 1));
+    if (benefit < prices[sellIdx] - prices[buyIdx]) benefit = prices[sellIdx] - prices[buyIdx];
     
-    if (curMax < curMin){
-      continue;
-    }
-    
-    benefits.push(curMax - curMin);
-    
-    let nextMin = null;
-    for (let j = i + 1; j < prices.length - 1; j++) {
-      if (prices[j] < curMin) {
-        target = j
-        nextMin = prices[j];
-        break;
-      }
-    }
-    
-    if (nextMin === null) {
-      break;
-    }
+    sellIdx++;
   }
   
-  return Math.max(...benefits) > 0 ? Math.max(...benefits) : 0 ;
+  return benefit;
 };
