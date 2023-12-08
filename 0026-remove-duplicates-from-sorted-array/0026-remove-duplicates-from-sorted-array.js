18fa51753fe43b18b22e2e4d16dbce7c05f5c474
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-12-08 22:20:40 +0900

    Time: 70 ms (34.22%), Space: 44.2 MB (82.13%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
+const removeDuplicates = function(nums) {
   let swipIdx = 0;
   
   for (let i = 1; i < nums.length; i++) {
