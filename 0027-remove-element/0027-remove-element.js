352b3578548ec8a5932cb0aaa5a5e71b991b804c
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-12-11 23:55:05 +0900

    Time: 57 ms (26.76%), Space: 41.3 MB (97.29%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function(nums, val) {
+  let count = 0;
+  
+  let target = nums.length - 1;
+  let curPoint = 0;
+  
+  while (nums.indexOf(val) !== -1) {
+    if (nums[curPoint] === val) {
+      const replaceNum = nums[target];
+      nums[curPoint] = replaceNum;
+      nums[target] = NaN;
+      target--;
+      count++;
+    } else {
+      curPoint++;
+    }
+  }
+
+  return nums.length - count;
+};
