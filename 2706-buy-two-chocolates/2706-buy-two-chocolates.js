48af711c1519d0afafb26265bcbc21ed299f1b87
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-12-21 00:01:15 +0900

    Time: 77 ms (88.15%), Space: 45.9 MB (84.07%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.js
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.js
@@ -3,20 +3,20 @@
  * @param {number} money
  * @return {number}
  */
-var buyChoco = function(prices, money) {
+const buyChoco = function(prices, money) {
   let leftOver = -1;
     for(let i = 0; i < prices.length - 1; i++) {
       for (let j = 1; j < prices.length; j++) {
         if (i === j) continue;
         
-        if ((prices[i] + prices[j]) <= money) {
-          
+        if ((prices[i] + prices[j]) <= money) {   
           const curLeft = money - (prices[i] + prices[j]);
+          
           if (leftOver === -1) leftOver = curLeft;
           if (curLeft > leftOver) leftOver = curLeft;
         }
       }
     }
   
-  return leftOver >= 0 ? leftOver : money
+  return leftOver >= 0 ? leftOver : money;
 };
