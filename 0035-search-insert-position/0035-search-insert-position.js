1c99b770e7841e94d9df60e2a1fbd4ebcf3b7124
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-12-21 23:44:26 +0900

    Time: 56 ms (27.87%), Space: 44.7 MB (5.21%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  const pivot = Math.floor(nums.length / 2);
+  let curIdx = pivot;
+  
+  if (nums[0] >= target) return 0;
+  
+  if (nums.at(-1) < target) return nums.length;
+  
+  if (nums.at(-1) === target) return nums.length - 1;
+  
+  while (true) {
+    console.log(curIdx);
+    if (curIdx < 0 || curIdx >= nums.length) break;
+    
+    if (nums[curIdx] === target) break; 
+    
+    if (nums[curIdx] > target) {
+      if (nums[curIdx - 1] < target) {
+        // curIdx;
+        break;
+      };
+      
+      curIdx--;
+      continue;
+    }
+    
+    if (nums[curIdx] < target) {
+      if (nums[curIdx + 1] > target) {
+        curIdx++;
+        break;
+      };
+      
+      curIdx++;
+      continue;
+    }
+  }
+  
+  return curIdx;
+};
