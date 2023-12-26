d111e22ddccbc041d63c5f853cd67828b8f6ed0d
sewonjun <93499071+sewonjun@users.noreply.github.com>
2023-12-26 21:32:13 +0900

    Time: 196 ms (5.07%), Space: 65.4 MB (5.35%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.js
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+  let reversedList = {};
+  let headCopy = head;
+  let count = 0; 
+  while (headCopy?.val) {
+    const newNode = {val: headCopy.val, next:null };
+    newNode.next = reversedList;
+    reversedList = newNode;
+    headCopy = headCopy.next;
+    count++;
+  }
+
+  let newNode = head;
+
+  while (count > 0) {
+    const temp = newNode.next;
+    console.log("count", count);
+//     if (newNode.val === reversedList?.val) {
+//       newNode.next = null;
+       
+//       break;
+//      };
+      if (count === 1) {
+        newNode.next = null;
+
+        break;
+     };
+    
+//     if(newNode.next.val === reversedList.val) {
+//       newNode.next = {val: reversedList.val, next: null};
+      
+//       break;
+//     }
+    
+    if(count === 2) {
+      newNode.next = {val: reversedList.val, next: null};
+      
+      break;
+    }
+    
+    newNode.next = {val: reversedList.val, next: temp};
+    newNode = newNode.next.next;
+    count -= 2;
+    reversedList = reversedList.next;
+  }
+
+};
+
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
