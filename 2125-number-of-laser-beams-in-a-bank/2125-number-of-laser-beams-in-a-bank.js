cdc8ffa3b9acdf8c12cc0ca0ff73e894c0fe1ea8
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-01-03 11:15:06 +0900

    Time: 100 ms (36.47%), Space: 52.8 MB (17.65%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} bank
+ * @return {number}
+ */
+var numberOfBeams = function(bank) {
+  let result = 0;
+  let currentBeamCount = null;
+  
+  bank.forEach((arr) => {
+    const beamCount = arr.split("").filter(el => el === "1").length;
+
+    if (beamCount > 0) {
+      if (currentBeamCount === null) {
+        currentBeamCount = beamCount;
+      } else {
+       result += currentBeamCount * beamCount;
+       currentBeamCount = beamCount;
+      }
+    }
+  })
+  
+  return result;
+};
