c14532243d78239070dcc8ffa37b90c08cc13df6
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-01-15 16:03:21 +0900

    Time: 64 ms (36.88%), Space: 44.6 MB (33.43%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.js
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -11,15 +11,10 @@
  * @return {boolean}
  */
 const isSymmetric = function(root) {
-  //root외에 다른 node가 없을 시.
   if (!root.left && !root.right) return true;
-  
-  //위에가 통과되면, depth 2부터 시작한다는 말.. 
-  //만약 root 다음 depth부터 값이 다르다면, false; 
-  //하나의 값이 undefined여도,false는 나온다. 
+
   if (root.left?.val !== root.right?.val) return false;
-  
-  //depth 2번에서 node가 끝난다면? 
+
   if (!root.left.left && !root.left.right && !root.right.left && !root.right.left) {
     return true;
   }
@@ -27,40 +22,25 @@ const isSymmetric = function(root) {
   let outer = null;
   let inner = null;
   
-      
-  // if (root.left?.left && root.right?.right) {
-    outer = checkMirror(root.left.left, root.right.right);
-    console.log("outer", outer);
-  // } else {
-  //   console.log("outer 없음 ");
-  // }
-  
-  // if (root.left?.right && root.right?.left) {
-    inner = checkMirror(root.left.right, root.right.left);
-    console.log("inner", inner)
-  // } else {
-  //   console.log("inner 없음 ")
-  // }
-  
+  outer = checkMirror(root.left.left, root.right.right);
+  inner = checkMirror(root.left.right, root.right.left);
 
   return inner && outer;
 };
 
 
 function checkMirror(node1, node2) {
-  console.log(node1, node2);
   if (node1 === null && node2 === null) return true;
+  
   if (node1 === null || node2 === null) return false;
   
-  if (node1.val !== node2.val) {
-    return false;
-  }
+  if (node1.val !== node2.val) return false;
   
   const node1Left = node1?.left;
   const node2Right = node2?.right;
   const node1Right = node1?.right;
   const node2Left = node2?.left;
-  
+
   if (!node1Left && !node2Right && !node1Right && !node2Left) {
     return true;
   }
