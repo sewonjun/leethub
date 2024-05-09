8460707148afe24843123f68bd1f68bf136f8761
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-05-09 23:50:41 +0900

    Time: 85 ms (8.81%), Space: 55.3 MB (99.94%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = function(nums) {
+  let largestSum = Math.max(...nums); 
+  let curSum = 0;
+  nums.forEach(num => {
+    curSum += num;
+    
+    if (curSum > largestSum) largestSum = curSum;
+    
+    if (curSum < 0) curSum = 0;
+  })
+  
+  return largestSum;
+};
