f2803cd147e389e0311bafb4bad489e4c8dfa8e2
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-05-15 22:25:34 +0900

    Time: 123 ms (19.51%), Space: 49.5 MB (70.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,26 +3,6 @@
  * @param {number} target
  * @return {number[]}
  */
-// var twoSum = function(nums, target) {
-//     let returnVal = [];
-//     for (let i = 0; i < nums.length - 1; i++) {
-//         const firstTarget = nums[i];
-
-//         for (let j = i + 1; j < nums.length; j++) {
-//             const secondTarget = nums[j];
-
-//             if ( Number(firstTarget) + Number(secondTarget) === target) {
-//                 returnVal = [i ,j];
-
-//                 break;
-//             }
-//         }
-//     }
-
-//     return returnVal;
-// };
- 
-
 const twoSum = function(nums, target) {
   let firstIndex = 0;
   let lastIndex = nums.length - 1;
