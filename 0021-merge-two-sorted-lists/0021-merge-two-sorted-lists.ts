b82a00d4f257286999391de1a8d6ad28901e5dcb
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-05-20 02:28:43 +0900

    Time: 56 ms (94.66%), Space: 52.2 MB (73.62%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  const mergedList = new ListNode();
+  let curNode = mergedList;
+  let curList1 = list1;
+  let curList2 = list2;
+  
+  while (curList1 || curList2) {
+    if (curList1 === null) {
+      curNode.next = curList2;
+      curList2 = null;
+      
+      break;
+    }
+    
+    if (curList2 === null) {
+      curNode.next = curList1;
+      curList1 = null;
+      
+      break;
+    }
+    
+    if (curList1?.val === curList2?.val) {
+      curNode.next = new ListNode(curList1.val);
+      curNode = curNode.next;
+      curNode.next = new ListNode(curList2.val);
+      curNode = curNode.next;
+
+      curList1 = curList1.next;
+      curList2 = curList2.next;
+      
+      continue;
+    }
+
+    
+    if (curList1?.val < curList2?.val) {
+      
+      curNode.next = new ListNode(curList1.val);
+      curNode = curNode.next;
+
+      curList1 = curList1.next;
+      
+      continue;
+    }
+    
+    if (curList1?.val > curList2?.val) {
+      curNode.next = new ListNode(curList2.val);
+      curNode = curNode.next;
+
+      curList2 = curList2.next;
+    }
+  }
+  
+  return mergedList.next;
+};
