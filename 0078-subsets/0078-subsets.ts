bea99ad9a15e9c5d79c0f28868bcee860c6d8971
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-05-30 23:36:47 +0900

    Time: 63 ms (74.55%), Space: 53.3 MB (84.12%) - LeetHub

---
+++ 0078-subsets/0078-subsets.ts
@@ -0,0 +1,27 @@
+function subsets(nums: number[]): number[][] {
+    //origin은 []으로 시작한다. 
+    //subset은 순서는 중요하지 않음으로 
+    //재귀를 돌때, 앞에서부터 해당 index뒤만 보내준다. 
+    const result = getSubsets([], nums);
+
+    return [[], ...result];
+  };
+
+function getSubsets(origin: [] | number[], subArray: number[]) {
+  if (subArray.length === 1) return [[...origin, ...subArray]];
+
+  const ableCombination = [];
+
+  for(let i = 0; i < subArray.length; i++) {
+    const restArray = subArray.slice(i + 1); 
+    ableCombination.push([...origin, subArray[i]]);
+;
+    if (restArray.length) {
+      const nextCombination = getSubsets([...origin, subArray[i]], restArray);
+
+      ableCombination.push(...nextCombination);
+    }
+  }
+  
+  return ableCombination;
+}
