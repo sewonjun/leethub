eab6d2a7b23bb8157c7a39283bfc92cd28e69788
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-06-09 23:16:37 +0900

    Time: 101 ms (100.00%), Space: 66.7 MB (100.00%) - LeetHub

--- 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.ts
+++ 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.ts
@@ -6,10 +6,10 @@ function findWinningPlayer(skills: number[], k: number): number {
   const maxNum = Math.max(...skills);
   
   let i = 1;
+  
   while(winCount < k) {
     const secondPlayer = skills[i];
-    
-    //가장 큰 숫자가 가장 앞자리에 도달했다면, 그 뒤로는 무조건 이기는 싸움. 
+
     if (winner === maxNum) break;
     
     if (winner < secondPlayer) {
@@ -24,7 +24,3 @@ function findWinningPlayer(skills: number[], k: number): number {
 
   return winIndex;
 };
-
-//1, 3, 7, 2, 4. k : 5
-
-// 한번 순회를 마치면 무조건 가장 큰 수가 winner기 때문에, 그 뒤에 있는 k 들은 채우지 않아도 된다. 그냥 무조건 큰놈 return 하면 됨. 
