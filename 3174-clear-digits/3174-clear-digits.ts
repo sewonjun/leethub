6411ffa720b447333a9d5c80600a9ff3a755b36d
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-06-09 22:47:51 +0900

    Time: 60 ms (100.00%), Space: 52.3 MB (100.00%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.ts
+++ 3174-clear-digits/3174-clear-digits.ts
@@ -1,15 +1,19 @@
 function clearDigits(s: string): string {
-  const stack = [];
+  // const stack = [];
+  let stack = "";
   
   for (let i = 0; i < s.length; i++) {
     const char = parseInt(s[i], 10);
     
     if (isNaN(char)) {
-      stack.push(s[i]);
+      // stack.push(s[i]);
+      stack = stack + s[i]
     } else {
-      stack.pop();
+      // stack.pop();
+      stack = stack.slice(0, -1);
     }
   }
   
-  return stack.join("");
+  // return stack.join("");
+  return stack;
 };
