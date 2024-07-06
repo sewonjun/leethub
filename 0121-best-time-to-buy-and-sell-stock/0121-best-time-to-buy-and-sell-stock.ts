40ccd9f229939bae24e4e1fefcb4e66979ddbf6f
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-07-06 10:59:29 +0900

    Time: 1716 ms (5.08%), Space: 69.1 MB (5.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,15 +1,15 @@
 function maxProfit(prices: number[]): number {
   let result = 0;
   let minBuy = prices[0];
-  let maxSell = prices[1];
-  
+    
   for (let i = 1; i < prices.length; i ++) {
-    result = Math.max(prices[i] - minBuy, result);
+    console.log(minBuy, result);
     minBuy = Math.min(prices[i], minBuy);
+    result = Math.max(prices[i] - minBuy, result);
+     // minBuy = Math.min(prices[i], minBuy);
+    
+    // console.log(minBuy, result, "------------")
   }
   
   return result;
 };
-
-//앞에가 뒤에보다 크면, 무효 
-// 
