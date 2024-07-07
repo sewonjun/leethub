2b9718363c32a2fbbb75ba4dc6885441a3d1307f
sewonjun <93499071+sewonjun@users.noreply.github.com>
2024-07-07 10:37:33 +0900

    Time: 2972 ms (5.89%), Space: 49.4 MB (5.76%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+      let pivot = Math.ceil(n / 2);
+      let badVersion;
+      
+      while(true) {
+        if (!isBadVersion(pivot) && badVersion) {
+          break;
+        }
+        if (isBadVersion(pivot)) {
+          //이미 나쁜 버전은 찾았고, 시작점을 찾아야 함.
+          badVersion = true;
+          
+          pivot--;
+        } else {
+          console.log(pivot);
+          pivot = pivot + Math.ceil((n - pivot)/2);
+          // pivot++;
+        }
+      }
+      
+      return pivot + 1;
+    };
+};
